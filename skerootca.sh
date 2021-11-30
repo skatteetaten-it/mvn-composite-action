1848ee282da355b5be8e12ca2ee1c821e02c8886
Sverre VmFsc2tyw6U= <Sverre.Valskra@skatteetaten.no>
2021-11-30 13:29:50 +0100

    fikset skerootca

--- skerootca.sh
+++ skerootca.sh
@@ -2,11 +2,14 @@ echo "Hello World"
 if [ -z "$1" ]
   then
     echo "Ingen parametre"
+    mkdir -p ./bindings/ca-certificates
+    echo "ca-certificates" > ./bindings/ca-certificates/type
+    echo "$SKEROOTCA" > ./bindings/ca-certificates/SKEROOTCA.pem
 fi
 for ((i = 1; i <= $#; i++ )); do
   printf '%s\n' "Arg $i: ${!i}"
+  mkdir -p ./$1/bindings/ca-certificates
+  echo "ca-certificates" > ./$1/bindings/ca-certificates/type
+  echo "$SKEROOTCA" > ./$1/bindings/ca-certificates/SKEROOTCA.pem
 done
 
-#mkdir -p ./$1/bindings/ca-certificates
-#echo "ca-certificates" > ./$1/bindings/ca-certificates/type
-#echo "$SKEROOTCA" > ./$1/bindings/ca-certificates/SKEROOTCA.pem

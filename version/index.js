c22d369d452fa605bf20d2dd2e3e928a43d873de
Sverre VmFsc2tyw6U= <Sverre.Valskra@skatteetaten.no>
2021-12-09 09:26:39 +0100

    Lagt til metode for å hente ut versjon

--- version/index.js
+++ version/index.js
@@ -1,15 +1,33 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+function findMatch(ref, searchParam) {
+  let match = ref.match(searchParam);
+  return match.length == 0 ? null : match[1];
+}
+
+function run() {
+  let ref = github.context.ref;
+  const type = core.getInput("type", { required: true });
+  let version;
+  if (type == "release") {
+    version = findMatch("ref/tags/(.*)");
+  }
+  if (type == "branch") {
+    version = findMatch("ref/heads/(.*)");
+  } else {
+    core.setFailed("Invalid type: " + type + " (type is: branch, release)");
+    return;
+  }
+
+  if (!version) {
+    core.setFailed("Could not find version: " + type + " (ref: " + ref + ")");
+  }
+  core.setOutput("version", version);
+}
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  run();
 } catch (error) {
   core.setFailed(error.message);
 }

24f803af12e019602c7d32f8cdb8cab61aab192d
Sverre VmFsc2tyw6U= <Sverre.Valskra@skatteetaten.no>
2022-03-23 10:26:42 +0100

    Endret skerootca til runner_temp

--- setup/skerootca.sh
+++ setup/skerootca.sh
@@ -1,4 +1,4 @@
-echo "Legger til /tmp/bindings/ca-certificates/SKEROOTCA.pem"
-mkdir -p /tmp/bindings/ca-certificates
-echo "ca-certificates" > /tmp/bindings/ca-certificates/type
-echo "$SKEROOTCA" > /tmp/bindings/ca-certificates/SKEROOTCA.pem
+echo "Legger til $RUNNER_TEMP/bindings/ca-certificates/SKEROOTCA.pem"
+mkdir -p $RUNNER_TEMP/bindings/ca-certificates
+echo "ca-certificates" > $RUNNER_TEMP/bindings/ca-certificates/type
+echo "$SKEROOTCA" > $RUNNER_TEMP/bindings/ca-certificates/SKEROOTCA.pem

bf636a7752e69f99d8f6d37634bb3767e15e1b6f
Sverre VmFsc2tyw6U= <Sverre.Valskra@skatteetaten.no>
2022-03-23 13:10:29 +0100

    Endret tilbake til å lage skeroot under basedir

--- setup/skerootca.sh
+++ setup/skerootca.sh
@@ -1,4 +1,13 @@
-echo "Legger til /tmp/bindings/ca-certificates/SKEROOTCA.pem"
-mkdir -p /tmp/bindings/ca-certificates
-echo "ca-certificates" > /tmp/bindings/ca-certificates/type
-echo "$SKEROOTCA" > /tmp/bindings/ca-certificates/SKEROOTCA.pem
+if [ -z "$1" ]
+  then
+    echo "Legger til ./bindings/ca-certificates/SKEROOTCA.pem"
+    mkdir -p ./bindings/ca-certificates
+    echo "ca-certificates" > ./bindings/ca-certificates/type
+    echo "$SKEROOTCA" > ./bindings/ca-certificates/SKEROOTCA.pem
+fi
+for ((i = 1; i <= $#; i++ )); do
+  echo "Legger til ./${!i}/bindings/ca-certificates/SKEROOTCA.pem"
+  mkdir -p ./${!i}/bindings/ca-certificates
+  echo "ca-certificates" > ./${!i}/bindings/ca-certificates/type
+  echo "$SKEROOTCA" > ./${!i}/bindings/ca-certificates/SKEROOTCA.pem
+done
